ba7955a92636fb4c3074ef51edd2295475fbb05d
Denis Skripov <nizhikebinesi@gmail.com>
2017-10-13 21:40:45 +0600

    Add some description for new problem in package-info

---
+++ src/main/java/com/examples/skripov/sippo/lesson2/package-info.java
@@ -0,0 +1,15 @@
+package com.examples.skripov.sippo.lesson2;
+
+/*
+Problem about max cargo flow
+
+cities: c1, c2, cn
+
+dij - throughout
+
+transporting only on day(not night)
+
+volumes of stock: w2, ..., w(n-1)
+
+?: find sizes of flows between cities if from c1 to cn things transports every day.
+*/
